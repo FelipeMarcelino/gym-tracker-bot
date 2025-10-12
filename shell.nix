8beaa7ba7b5382933ea3788a32d46d595f8b138f
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-10-11 21:13:42 -0300

    first commit

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  stdenv,
+  ...
+}:
+let
+  pkgs-unfree = import pkgs.path {
+    inherit (pkgs) system;
+    config.allowUnfree = true;
+  };
+  pythonPackages = pkgs-unfree.python312Packages;
+in
+pkgs-unfree.mkShell {
+  buildInputs = [
+    pythonPackages.python
+    pythonPackages.venvShellHook
+    pkgs-unfree.autoPatchelfHook
+    pythonPackages.python-telegram-bot
+    pythonPackages.python-dotenv
+  ];
+  venvDir = "./.venv";
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH    
+    autoPatchelf ./.venv   '';
+  postShellHook = ''
+    unset SOURCE_DATE_EPOCH     
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [ stdenv.cc.cc ]}:$LD_LIBRARY_PATH '';
+}
